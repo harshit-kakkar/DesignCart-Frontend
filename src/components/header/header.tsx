fde49e93e5b0dec82fc5862c1030161bc996d0b5
harshit-kakkar <kharshit0@gmail.com>
2021-01-01 23:50:47 +0530

    Created skeleton layout for header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,8 +3,25 @@ import './header.css';
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className="header">
+      <div className="header-logo">
+        DESIGN CART
+      </div>
+      <div className="header-search">
+        SEARCH
+      </div>
+      <div className="header-links">
+        <div className="single-header-link">
+          LOGIN
+        </div>
+        <div className="single-header-link">
+          SIGNUP
+        </div>
+        <div className="single-header-link">
+          CART
+        </div>
+        
+      </div>
     </div>
   );
 }
